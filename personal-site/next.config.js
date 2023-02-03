24365fe56c019e23ac5a34f9d40049b8c6291b89
Jory Shilmover <jory@jaik.solutions>
2023-02-03 11:31:17 -0800

    Initial Next.js setup

---
+++ personal-site/next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  assetPrefix: "",
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;

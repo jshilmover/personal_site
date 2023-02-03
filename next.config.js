8cc89b97b9ca887e6ea4c16a933f22bf27c08226
Jory Shilmover <jory.shilmover@gmail.com>
2023-02-03 15:37:26 -0800

    config fixes

--- next.config.js
+++ next.config.js
@@ -2,28 +2,6 @@
 const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 
 const nextConfig = {
-  webpack: (config, options) => {
-    config.module.push({
-      resolve: {
-        plugins: [new TsconfigPathsPlugin({ configFile: "./tsconfig.json" })],
-      },
-    });
-
-    config.module.loaders = [
-      ...config.module.loaders,
-      {
-        test: /\.css$/,
-        loader: "style-loader!css-loader",
-      },
-    ];
-
-    config.module.resolve.extensions = [
-      ...config.module.resolve.extensions,
-      ".css",
-    ];
-
-    return config;
-  },
   reactStrictMode: true,
 };
 

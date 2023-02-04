c01db708b34c5314614332bda1fe0d2952afb7f6
Jory Shilmover <jory.shilmover@gmail.com>
2023-02-03 19:51:34 -0800

    PageContainer rename

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { useEffect } from "react";
-import Header from "@/components/Structural/Header";
+import PageContainer from "@/components/Structural/PageContainer";
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
@@ -12,8 +12,8 @@ export default function App({ Component, pageProps }: AppProps) {
   }, []);
 
   return (
-    <Header>
+    <PageContainer>
       <Component {...pageProps} />
-    </Header>
+    </PageContainer>
   );
 }
